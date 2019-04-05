73315f8cac31cf1f08f1b25eee405abdaa3f84eb
Heidi Williams-Foy <foywilliams@gmail.com>
2019-04-04 22:12:18 -0400

    got fetch working and images to display

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,19 +2,27 @@
 
 var url = "https://photorankapi-a.akamaihd.net/customers/215757/media/recent?version=v2.2&auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&rights_given=true"
 
-fetch('https://photorankapi-a.akamaihd.net/customers/215757/media/recent?version=v2.2&auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&rights_given=true')
-.then(resp => resp.json()).then(({ data }) => {
- return(data._embedded.media.map(m => m.images.mobile));
-});
-const createImageGrid = ({ data }) => {
-    $( function() {
-        var images = [],
-             li = $( '<li/>' ),
-             ul = $( '#image-grid > ul' );
-        $.each( images, function( i, v ) {
-          ul.append( li.clone().html( '<img src="' + v + '"/>' ) );
+const createImageGrid = (data) => {
+    //console.log(data);
+    jQuery( function() {
+        var li = jQuery( '<li/>' ),
+            ul = jQuery( '<ul/>' );
+        $.each( data, function( i, v ) {
+          ul.append( li.clone().html( '<img src="' + v.images.normal + '"/>' ) );
         });
+        console.log(ul)
+        $('body').append(ul);
     //a for loop iterates through the image url array
     //and appends them to DOM in the proper div elements
 });
-fetch(url).then(resp => resp.json()).then(createImageGrid);
+};
+
+fetch(url)
+.then((resp) => { 
+  return resp.json();
+})
+.then((resp_json) => { 
+  data = JSON.parse(JSON.stringify(resp_json));
+  console.log(data);
+  createImageGrid(data.data._embedded.media)
+});
